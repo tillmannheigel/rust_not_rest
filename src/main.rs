481c6e350df27226b6061f4012d3f5a9844ba01a
Tillmann Heigel <tillmann.heigel@idealo.de>
2021-10-10 20:06:20 +0200

    finish guessing game and add server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
-    let mut guess = String::new();
+    println!("Guess a number!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let generated_number = rand::thread_rng().gen_range(1..101);
 
-    println!("You guessed: {}", guess);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 = guess.trim().parse().expect("Expect a number!");
+
+        match guess.cmp(&generated_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Equal => {
+                println!("YES, you found it!");
+                break;
+            },
+            Ordering::Greater => println!("too big!")
+        }
+
+        println!("You guessed: {}", guess);
+    }
 }
